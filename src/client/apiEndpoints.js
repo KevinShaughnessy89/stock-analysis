070c8161118b6a26240e46259186f96fe0519dae
Kevin Shaughnessy <kevinshaughnessy89@gmail.com>
2024-11-23 18:26:18 -0800

    this should work

--- src/client/apiEndpoints.js
+++ src/client/apiEndpoints.js
@@ -1,175 +1,173 @@
 import { useAuthStore } from "./authStore.js";
 
-const DOMAIN = process.env.REACT_APP_ENV === 'development' ? 'http://localhost:5000' : 'https://kevinshaughnessy.ca'
+const DOMAIN = "https://kevinshaughnessy.ca";
 
 export const apiEndpoints = {
-    getPriceData: { // Define endpoint base configuration
-        method: 'GET',
-        baseURL: `${DOMAIN}`,
-        endpoint: '/api/data/prices',
-        headers: {
-            'Content-Type': 'application/JSON'
-        },
-        params: {
-            startDate: true,
-            endDate: true,
-            symbol: true
-        },
-        responseHandlers: {
-            200: (response) => {
-                let finalOutput = [{ 
-                    id: "stockPrice",
-                    data: response.data.map(item => ({
-                        x: new Date(item.timestamp),
-                        y: item.high
-                    
-                    })
-                )}];
-                return finalOutput;
-            }
-        }
-    },
-    getPriceAverage: {
-        method: 'GET',
-        baseURL: DOMAIN,
-        endpoint: '/api/data/prices/average',
-        headers: {
-            'Content-Type': 'applications/JSON'
-        },
-        params: {
-            symbol: true,
-            startDate: true,
-            endDate: true
-        }
-    },
-    getRSS: {
-        method: 'GET',
-        baseURL: 'https://api.rss2json.com',
-        endpoint: '/v1/api.json',
-        headers: {
-            'Content-Type': 'applications/JSON'
-        },
-        params: {
-            rss_url: true,
-        },
-        appendParams: {
-            api_key: process.env.RSS_KEY,
-            count: 10
-        }
-    },
-    registerUser: {
-        method: 'POST',
-        baseURL: DOMAIN,
-        endpoint: `/api/user/register`,
-        data: {
-            username: true,
-            password: true,
-            confirmPassword: true,
-            email: true
-        }
-    },
-    loginUser: {
-        method: 'POST',
-        baseURL: DOMAIN,
-        endpoint: `/api/user/login`,
-        data: {
-            username: true,
-            password: true
-        },
-        responseHandlers: {
-            200: (response) => {
-                useAuthStore.getState().setIsAuthenticated(true);
-                return response.success;
-            }
-        }
-    },
-    logoutUser: {
-        method: 'POST',
-        baseURL: DOMAIN,
-        endpoint: '/api/user/logout',
-        responseHandlers: {
-            200: (response) => {
-                useAuthStore.getState().setIsAuthenticated(false);
-            }
-        }
-    },
-    verifyCookie: {
-        method: 'GET',
-        baseURL: DOMAIN,
-        endpoint: '/api/auth/verify',
-        optional: {
-            credentials: 'include'
-        },
-        responseHandlers: {
-            200: (response) => {
-                useAuthStore.getState().setIsAuthenticated(true);
-                return response.success;
-            }
-        }
-    },
-    getUserInfo: {
-        method: 'GET',
-        baseURL: DOMAIN,
-        endpoint: '/api/user/info',
-        params: {
-            fields: true
-        },
-        optional: {
-            credentials: 'include'
-        }
-    },
-    scrapeWebsite: {
-        method: 'POST',
-        baseURL: DOMAIN,
-        endpoint: '/api/scrape',
-        data: {
-            url: true
-        }
-    },
-    getStockSymbols: {
-        method: 'GET',
-        baseURL: DOMAIN,
-        endpoint: '/api/data/symbols',
-        params: {
-
-        }
-    },
-    queryNews: {
-        method: 'GET',
-        baseURL: 'https://www.alphavantage.co?',
-        endpoint: '/query',
-        params: {
-            topics: true,
-            tickers: true,
-            function: true,
-        },
-        appendParams: {
-            apikey:  process.env.ALPHA_VANTAGE_KEY,
-        }
-    },
-    getUserPreferences: {
-        method: 'GET',
-        baseURL: DOMAIN,
-        endpoint: '/api/user/preferences',
-        params: {
-
-        },
-        optional: {
-            credentials: 'include'
-        }
-    },
-    saveFeedItem: {
-        method: 'POST',
-        baseURL: DOMAIN,
-        endpoint: '/api/user/feed',
-        data: {
-            title: true,
-            link: true,
-            source: true,
-            description: true
-        },
-        optional: {
-            credentials: 'include'
-        }
-    }
-}
+	getPriceData: {
+		// Define endpoint base configuration
+		method: "GET",
+		baseURL: `${DOMAIN}`,
+		endpoint: "/api/data/prices",
+		headers: {
+			"Content-Type": "application/JSON",
+		},
+		params: {
+			startDate: true,
+			endDate: true,
+			symbol: true,
+		},
+		responseHandlers: {
+			200: (response) => {
+				let finalOutput = [
+					{
+						id: "stockPrice",
+						data: response.data.map((item) => ({
+							x: new Date(item.timestamp),
+							y: item.high,
+						})),
+					},
+				];
+				return finalOutput;
+			},
+		},
+	},
+	getPriceAverage: {
+		method: "GET",
+		baseURL: DOMAIN,
+		endpoint: "/api/data/prices/average",
+		headers: {
+			"Content-Type": "applications/JSON",
+		},
+		params: {
+			symbol: true,
+			startDate: true,
+			endDate: true,
+		},
+	},
+	getRSS: {
+		method: "GET",
+		baseURL: "https://api.rss2json.com",
+		endpoint: "/v1/api.json",
+		headers: {
+			"Content-Type": "applications/JSON",
+		},
+		params: {
+			rss_url: true,
+		},
+		appendParams: {
+			api_key: process.env.RSS_KEY,
+			count: 10,
+		},
+	},
+	registerUser: {
+		method: "POST",
+		baseURL: DOMAIN,
+		endpoint: `/api/user/register`,
+		data: {
+			username: true,
+			password: true,
+			confirmPassword: true,
+			email: true,
+		},
+	},
+	loginUser: {
+		method: "POST",
+		baseURL: DOMAIN,
+		endpoint: `/api/user/login`,
+		data: {
+			username: true,
+			password: true,
+		},
+		responseHandlers: {
+			200: (response) => {
+				useAuthStore.getState().setIsAuthenticated(true);
+				return response.success;
+			},
+		},
+	},
+	logoutUser: {
+		method: "POST",
+		baseURL: DOMAIN,
+		endpoint: "/api/user/logout",
+		responseHandlers: {
+			200: (response) => {
+				useAuthStore.getState().setIsAuthenticated(false);
+			},
+		},
+	},
+	verifyCookie: {
+		method: "GET",
+		baseURL: DOMAIN,
+		endpoint: "/api/auth/verify",
+		optional: {
+			credentials: "include",
+		},
+		responseHandlers: {
+			200: (response) => {
+				useAuthStore.getState().setIsAuthenticated(true);
+				return response.success;
+			},
+		},
+	},
+	getUserInfo: {
+		method: "GET",
+		baseURL: DOMAIN,
+		endpoint: "/api/user/info",
+		params: {
+			fields: true,
+		},
+		optional: {
+			credentials: "include",
+		},
+	},
+	scrapeWebsite: {
+		method: "POST",
+		baseURL: DOMAIN,
+		endpoint: "/api/scrape",
+		data: {
+			url: true,
+		},
+	},
+	getStockSymbols: {
+		method: "GET",
+		baseURL: DOMAIN,
+		endpoint: "/api/data/symbols",
+		params: {},
+	},
+	queryNews: {
+		method: "GET",
+		baseURL: "https://www.alphavantage.co?",
+		endpoint: "/query",
+		params: {
+			topics: true,
+			tickers: true,
+			function: true,
+		},
+		appendParams: {
+			apikey: process.env.ALPHA_VANTAGE_KEY,
+		},
+	},
+	getUserPreferences: {
+		method: "GET",
+		baseURL: DOMAIN,
+		endpoint: "/api/user/preferences",
+		params: {},
+		optional: {
+			credentials: "include",
+		},
+	},
+	saveFeedItem: {
+		method: "POST",
+		baseURL: DOMAIN,
+		endpoint: "/api/user/feed",
+		data: {
+			title: true,
+			link: true,
+			source: true,
+			description: true,
+		},
+		optional: {
+			credentials: "include",
+		},
+	},
+};
